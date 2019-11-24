42d0e60a8f94153226058c1aa98635032430510f
Alejo Castaño <drskantus@hotmail.com>
2019-11-25 00:47:29 +0100

    Add view component

--- index.js
+++ index.js
@@ -1,6 +1,39 @@
+import React from "react";
+import { View, requireNativeComponent } from "react-native";
 
-import { NativeModules } from 'react-native';
+const RNRoundedProgressBar = requireNativeComponent("RoundedProgressBar");
 
-const { RNRoundedProgressBar } = NativeModules;
+const RoundedProgressBar = props => {
+  const { percent, borderWidth, color } = props;
 
-export default RNRoundedProgressBar;
+  const options = {
+    percent: percent || 1,
+    borderWidth: borderWidth || 4,
+    size: props.size || 50,
+    color: color || "#c2c2c2"
+  };
+
+  return (
+    <View
+      style={{
+        width: 100,
+        height: 100,
+        justifyContent: "center",
+        alignItems: "center"
+      }}
+    >
+      <View
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0
+        }}
+      >
+        <RNRoundedProgressBar props={options} />
+      </View>
+      {props.children}
+    </View>
+  );
+};
+
+export default RoundedProgressBar;
